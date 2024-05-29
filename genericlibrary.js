98069c29b7600a81eb7d0b836152f61de60f0f23
AzariasANeals <Azarias.P.Aneals@seattlecolleges.edu>
2024-05-28 19:25:38 -0700

    Final Update

--- genericlibrary.js
+++ genericlibrary.js
@@ -1,40 +1,77 @@
-// Part 1 : Generic Identity Function
+/* Part 1 : Generic Identity Function
+This is a simple identity function that takes an argument
+of type T and returns a value of type T.
+
+The outputs below for output and output 2 are
+
+Hello
+1
+*/
 function identity(arg) {
     return arg;
 }
 var output = identity("Hello");
-console.log(output); //Output: hello
+console.log(output); //Output: Hello
 var output2 = identity(1);
-console.log(output2);
-// Part 2: Generic Array Reversal Function
+console.log(output2); // Output: 1
+/* Part 2: Generic Array Reversal Function
+This is a simple function that takes in an array of elements
+and returns the array with the elements in reverse order
+
+The output for the reverseArray is [5,4,3,2,1]
+*/
 function reverseArray(array) {
     return array.reverse();
 }
 var numbers = [1, 2, 3, 4, 5];
 var reversedNumbers = reverseArray(numbers);
 console.log(reversedNumbers); // Output: [5,4,3,2,1]
-// Part 3: Generic Object Mapper
-// function mapObject<K, V, U>
-//Part 4: Generic Filtering Function
-/*function filterArray<T>(array: T[]): T[]{
-    return
-}
-*/
-function start() {
-    return (Car);
-}
-;
-var Car = /** @class */ (function () {
-    function Car(engine, color) {
-        this.engine = engine;
-        this.color = color;
-        start();
-        {
-            console.log("Started engine: ".concat(this.engine, ", Color: ").concat(this.color));
-        }
-        ;
+var numeric = /** @class */ (function () {
+    function numeric() {
+        this.value = {};
     }
-    ;
-    return Car;
+    numeric.prototype.getItem = function (key) {
+        return this.value[key];
+    };
+    numeric.prototype.setItem = function (key, item) {
+        this.value[key] = item;
+    };
+    return numeric;
 }());
-;
+var alpha = /** @class */ (function () {
+    function alpha() {
+        this.value = {};
+    }
+    alpha.prototype.getItem = function (key) {
+        return this.value[key];
+    };
+    alpha.prototype.setItem = function (key, item) {
+        this.value[key] = item;
+    };
+    return alpha;
+}());
+function mapObject(key, value, newValue) {
+    var buf = newValue;
+    var retrieved = value.getItem(key);
+    buf.setItem(key, retrieved);
+    newValue = buf;
+    return newValue;
+}
+var key = "14";
+var k = new numeric();
+var v = new alpha();
+k.setItem(key, 23);
+var nv = mapObject(key, k, v);
+console.log("Original class value for key ".concat(key, " is ").concat(v.getItem(key)));
+console.log("Value in new class object for key ".concat(key, " is ").concat(nv.getItem(key)));
+/*Part 4: Generic Filtering Function
+This function takes in an array of elements and will
+divide the array by 2 and will only return those who have a remainder of 0.
+*/
+var numberOne = [1, 2, 3, 4, 5, 6];
+function filterArray(array) {
+    var newFilter = array.filter(function (x) { return x % 2 === 0; });
+    console.log(newFilter);
+}
+filterArray(numberOne);
+// Output: [2, 4, 6]
--- genericlibrary.ts
+++ genericlibrary.ts
@@ -1,15 +1,29 @@
-// Part 1 : Generic Identity Function
+/* Part 1 : Generic Identity Function
+This is a simple identity function that takes an argument
+of type T and returns a value of type T.
+
+The outputs below for output and output 2 are 
+
+Hello
+1
+*/
+
 function identity<T>(arg: T): T {
     return arg;
 }
 
 let output = identity<string>("Hello");
-console.log(output); //Output: hello
+console.log(output); //Output: Hello
 
 let output2 = identity<number>(1);
-console.log(output2);
+console.log(output2); // Output: 1
 
-// Part 2: Generic Array Reversal Function
+/* Part 2: Generic Array Reversal Function
+This is a simple function that takes in an array of elements
+and returns the array with the elements in reverse order
+
+The output for the reverseArray is [5,4,3,2,1]
+*/
 function reverseArray<T>(array: T[]): T[]{
     return array.reverse();
 }
@@ -18,31 +32,71 @@ let numbers: number[] = [1,2,3,4,5];
 let reversedNumbers: number[] = reverseArray(numbers);
 console.log(reversedNumbers); // Output: [5,4,3,2,1]
 
-// Part 3: Generic Object Mapper
-// function mapObject<K, V, U>
 
-//Part 4: Generic Filtering Function
-/*function filterArray<T>(array: T[]): T[]{
-    return 
+
+/* Part 3: Generic Object Mapper
+This function is taking a class numeric object and returning a 
+class alpha object with the same value for the key. 
+
+Output is:
+Original class value for key 14 is 23
+Value in new class object for key 14 is 23
+*/
+interface Mapper<T>{
+    getItem(key: string): T;
+    setItem(key: string, item: T): void;
 }
+
+class numeric implements Mapper<number>{
+    private value: {[key: string]: number} = {};
+    getItem(key: string): number{
+        return this.value[key];
+    }
+    setItem(key: string, item: number): void {
+        this.value[key] = item;
+    }
+}
+
+class alpha implements Mapper<string>{
+    private value: {[key: string]: string} = {};
+    getItem(key: string): string{
+        return this.value[key];
+    }
+    setItem(key: string, item: string): void {
+        this.value[key] = item;
+    }
+}
+
+
+
+function mapObject<V, U>(key: string, value: Mapper<V>, newValue: Mapper<U>): Mapper<U> {
+    let buf: Mapper<U> = newValue ;
+    let retrieved: V = value.getItem(key);
+    buf.setItem(key, retrieved as undefined as U);
+    newValue = buf;
+    return newValue;
+}
+
+let key: string = "14";
+let k: numeric = new numeric();
+let v: alpha = new alpha();
+
+k.setItem(key, 23);
+let nv = mapObject(key, k, v);
+console.log(`Original class value for key ${key} is ${v.getItem(key)}`);
+console.log(`Value in new class object for key ${key} is ${nv.getItem(key)}`);
+
+
+/*Part 4: Generic Filtering Function
+This function takes in an array of elements and will
+divide the array by 2 and will only return those who have a remainder of 0.
 */
+let numberOne = [1,2,3,4,5,6];
 
-/*
-function start(){
-    return console.log(Car)
-};
+function filterArray<T>(array: T[]){
+    let newFilter = array.filter((x) => x as number % 2 ===0);
+    console.log(newFilter)
+}
+filterArray(numberOne);
 
-class Car {
-    private engine: string;
-    public color: string;
-
-    constructor(engine: string, color: string){
-        this.engine = engine;
-        this.color = color;
-
-    start() 
-        console.log(`Started engine: ${this.engine}, Color: ${this.color}`);
-        ;
-    };
-};
-*/
+// Output: [2, 4, 6]
